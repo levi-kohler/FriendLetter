39f0135e2ec6cbd5bee42714ee366b9aa2ceaf6c
Levi Kohler <kohler.la01@gmail.com>
2021-03-01 11:18:59 -0800

    small change to Startup.cs

--- FriendLetter/Startup.cs
+++ FriendLetter/Startup.cs
@@ -11,9 +11,9 @@ namespace FriendLetter
     public Startup (IWebHostEnvironment env)
     {
       var builder = new ConfigurationBuilder()
-      .SetBasePath(env.ContentRootPath)
-      .AddEnvironmentVariables();
-    Configuration = builder.Build();
+        .SetBasePath(env.ContentRootPath)
+        .AddEnvironmentVariables();
+      Configuration = builder.Build();
     }
 
     public IConfigurationRoot Configuration { get; }
